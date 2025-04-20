f1f42a109ef6028bda827e82965d9c58db3cc1e7
miftah <sultanmimore@gmail.com>
2025-04-20 14:49:33 +0300

    made some change

--- api/webhook.js
+++ api/webhook.js
@@ -1,10 +1,23 @@
-const bot = require('../bot'); // make sure path is correct
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token);
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     if (req.method === 'POST') {
-        bot.processUpdate(req.body);
+        const body = req.body;
+
+        // Reply to user
+        if (body.message) {
+            const chatId = body.message.chat.id;
+            const text = body.message.text;
+
+            await bot.sendMessage(chatId, `You said: ${text}`);
+        }
+
         return res.status(200).send('OK');
     } else {
-        res.status(200).send('Bot is running!');
+        res.status(405).send('Method Not Allowed');
     }
-}
+};
+
+
